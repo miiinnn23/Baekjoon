1eb5969d2cecb1c1734c65990119587d2e1c029b
Seung Min Lee <76769670+miiinnn23@users.noreply.github.com>
2022-08-11 14:28:03 +0900

    1325_failure version
    시간초과 + 로직틀림
    ㅜㅜ

---
+++ 2022-1 Study/1325.cpp
@@ -0,0 +1,51 @@
+#include <cstdio>
+#include <vector>
+#define MAX 10001
+
+using namespace std;
+
+int N, M;
+int maximum, temp;
+
+vector<int> computer[MAX];
+int counter[MAX];
+
+void DFS(int num, int start, int count);
+
+int main(void) {
+    scanf("%d %d", &N, &M);
+
+    for(int i = 0; i < M; i++) {
+        int num1, num2;
+        scanf("%d %d", &num1, &num2);
+
+        computer[num2].push_back(num1);
+    }
+
+    for(int i = 1; i <= N; i++) {
+        temp = 0;
+
+        DFS(i, i, 1);
+        counter[i] = temp;
+        maximum = (temp > maximum ? temp : maximum);
+    }
+
+    for(int i = 1; i <= N; i++) {
+        if(counter[i] == maximum)
+            printf("%d ", i);
+    }
+    
+    printf("\n");
+    return 0;
+}
+
+void DFS(int num, int start, int count) {
+    if(count != 1 && start == num) return;
+
+    temp = (count > temp ? count : temp);
+
+    for(int i = 0; i < computer[num].size(); i++) {
+        int next = computer[num][i];
+        DFS(next, start, count + 1);
+    }
+}
