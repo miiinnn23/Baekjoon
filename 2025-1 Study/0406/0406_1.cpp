157d1e5341d4ee11c02a0aa64b33c4ef3b0d9edf
miiinn23 <ansnnd123@naver.com>
2025-04-09 00:03:14 +0900

    refactor: 0406 문제풀이

--- 2025-1 Study/0406/0406_1.cpp
+++ 2025-1 Study/0406/0406_1.cpp
@@ -21,36 +21,6 @@ int dy[] = { 0, -1, 1, 0, 0 };
 vector<Shark> shark;
 vector<Shark> Map[MAX][MAX];
 
-pair<int, int> move1D(int x, int s, int effectiveDir, int N) {
-    if (N == 1) return {1, effectiveDir}; // 구간 크기 1이면 항상 1번 칸
-    
-    int period = 2 * (N - 1);  // 왕복 주기
-    s %= period;             // 속력을 주기 내에서만 고려
-    
-    int offset;
-    if (effectiveDir == +1) {
-        // 정방향: x-1 (0-based)
-        offset = x - 1;
-        offset = (offset + s) % period;
-    } else { // effectiveDir == -1
-        // 역방향: (x-1)에서 s를 뺀다.
-        offset = (x - 1) - s;
-        // C++에서 음수 모듈로 보정:
-        offset = (offset % period + period) % period;
-    }
-    
-    int new_x, newEffective;
-    if (offset < N) {
-        new_x = offset + 1;  // 다시 1-based 좌표로 변환
-        newEffective = +1;
-    } else {
-        new_x = period - offset + 1;
-        newEffective = -1;
-    }
-    
-    return {new_x, newEffective};
-}
-
 bool Compare(const Shark& a, const Shark& b) {
     if(a.Size > b.Size) return true;
     return false;
@@ -78,7 +48,6 @@ int main(void) {
 int Solution() {
     int answer = 0;
     for(int time = 1; time <= C; time++) {
-        cout << "Day " << time << endl;
         // 낚시꾼 이동
         for(int i = 1; i <= R; i++) {
             if(!Map[i][time].empty()) {
@@ -106,7 +75,6 @@ int Solution() {
                 if(Map[i][j].size() > 1) {
                     sort(Map[i][j].begin(), Map[i][j].end(), Compare);
                     for(int k = Map[i][j].size() - 1; k >= 1; k--) {
-                        cout << "사냥: " << Map[i][j][0].Size << " > " << Map[i][j][k].Size << endl;
                         int caught = Map[i][j][k].Index;
                         shark[caught].Size = -1;
                         Map[i][j].pop_back();
@@ -121,64 +89,29 @@ int Solution() {
 }
 
 void Movement() {
-    int period[2] = {2 * (R - 1), 2 * (C - 1)};
-    
     for(int i = 0; i < shark.size(); i++) {
         if(shark[i].Size < 0) continue;
-        // 1, 2: 상하, 3, 4: 좌우 -> 0: 상하이동, 1: 좌우이동
-        cout << "(" << shark[i].R << ", " << shark[i].C << "), 이동방향: " << shark[i].Dir << " -> ";
-        int shark_dir = (shark[i].Dir - 1) / 2 == 0 ? 0 : 1;
-
-        // int offset = 0, speed = 0, moved = 0;
-        // int new_pos, new_dir;
-        // if(shark[i].Dir == 1 || shark[i].Dir == 2) {
-        //     speed = shark[i].Speed % period[0];
-        //     offset = shark[i].Dir == 2 ? shark[i].R - 1 : ((R - 1) - (shark[i].R - 1));
-        //     moved = (offset + speed) % period[0];
-
-        //     if(moved < R) {
-        //         new_pos = moved + 1;
-        //         new_dir = shark[i].Dir;
-        //     }
-        //     else {
-        //         new_pos = period[0] - moved + 1;
-        //         new_dir = shark[i].Dir == 1 ? 2 : 1;
-        //     }
-        //     shark[i].R = new_pos;
-        //     shark[i].Dir = new_dir;
-        // }
-        // else {
-        //     speed = shark[i].Speed % period[1];
-        //     offset = shark[i].Dir == 3 ? shark[i].C - 1 : ((C - 1) - (shark[i].C - 1));
-        //     moved = (offset + speed) % period[1];
-
-        //     if(moved < C) {
-        //         new_pos = moved + 1;
-        //         new_dir = shark[i].Dir;
-        //     }
-        //     else {
-        //         new_pos = period[1] - moved + 1;
-        //         new_dir = shark[i].Dir == 3 ? 4 : 3;
-        //     }
-        //     shark[i].C = new_pos;
-        //     shark[i].Dir = new_dir;
-        // }
-
-        if (shark[i].Dir == 1 || shark[i].Dir == 2) {
-            int effectiveDir = (shark[i].Dir == 2) ? +1 : -1;  // 아래: +1, 위: -1
-            auto res = move1D(shark[i].R, shark[i].Speed, effectiveDir, R);
-            shark[i].R = res.first;
-            // 새 효과적 방향을 실제 방향으로 변환:
-            shark[i].Dir = (res.second == +1 ? 2 : 1);
+        // 1, 2: 상하, 3, 4: 좌우
+        int speed = shark[i].Speed;
+        int dir = shark[i].Dir;
+        
+        if(dir < 3) speed %= 2 * (R - 1);
+        else speed %= 2 * (C - 1);
+        
+        int r = shark[i].R, c = shark[i].C;
+        for(int s = 0; s < speed; s++) {
+            if(r + dy[dir] < 1 || r + dy[dir] > R || c + dx[dir] < 1 || c + dx[dir] > C) {
+                r -= dy[dir];
+                c -= dx[dir];
+                if(dir < 3) dir == 1 ? dir = 2 : dir = 1;
+                else dir == 3 ? dir = 4 : dir = 3;
+            }
+            else {
+                r += dy[dir], c += dx[dir];
+            }
         }
-        // 가로 이동 (방향 3: 오른쪽, 4: 왼쪽)
-        else if (shark[i].Dir == 3 || shark[i].Dir == 4) {
-            int effectiveDir = (shark[i].Dir == 3) ? +1 : -1;  // 오른쪽: +1, 왼쪽: -1
-            auto res = move1D(shark[i].C, shark[i].Speed, effectiveDir, C);
-            shark[i].C = res.first;
-            shark[i].Dir = (res.second == +1 ? 3 : 4);
-        }
-        cout << "(" << shark[i].R << ", " << shark[i].C << "), 이동방향 :" << shark[i].Dir << "\n";
+        shark[i].R = r, shark[i].C = c;
+        shark[i].Dir = dir;
 
         Map[shark[i].R][shark[i].C].push_back(shark[i]);
     }
